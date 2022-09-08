891b45f1bbbb0fd127c13f9f4f4c5b763e29b887
Prikhod322 <vovkaprikhod@gmail.com>
2022-09-08 18:41:25 +0300

    added file open, save, next & prev pages

--- SIPView PDF/Form1.Designer.cs
+++ SIPView PDF/Form1.Designer.cs
@@ -35,7 +35,10 @@
             this.nextPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.previousPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.imGearPageView1 = new ImageGear.Windows.Forms.ImGearPageView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -64,24 +67,28 @@
             this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
             this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // nextPageToolStripMenuItem
             // 
             this.nextPageToolStripMenuItem.Name = "nextPageToolStripMenuItem";
             this.nextPageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.nextPageToolStripMenuItem.Text = "Next Page";
+            this.nextPageToolStripMenuItem.Click += new System.EventHandler(this.nextPageToolStripMenuItem_Click);
             // 
             // previousPageToolStripMenuItem
             // 
             this.previousPageToolStripMenuItem.Name = "previousPageToolStripMenuItem";
             this.previousPageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.previousPageToolStripMenuItem.Text = "Previous Page";
+            this.previousPageToolStripMenuItem.Click += new System.EventHandler(this.previousPageToolStripMenuItem_Click);
             // 
             // imGearPageView1
             // 
@@ -98,12 +105,30 @@
             this.imGearPageView1.TabIndex = 1;
             this.imGearPageView1.UseConfiguredScrollbarIncrements = false;
             this.imGearPageView1.VerticalArrowIncerment = 1;
+            this.imGearPageView1.Load += new System.EventHandler(this.imGearPageView1_Load);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.imGearPageView1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -111,6 +136,8 @@
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,6 +152,8 @@
         private System.Windows.Forms.ToolStripMenuItem nextPageToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem previousPageToolStripMenuItem;
         private ImageGear.Windows.Forms.ImGearPageView imGearPageView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
 
--- SIPView PDF/Form1.cs
+++ SIPView PDF/Form1.cs
@@ -1,20 +1,163 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+
+using ImageGear.Core;
+using ImageGear.Display;
+using ImageGear.Formats;
+using ImageGear.Formats.PDF;
+using ImageGear.Evaluation;
 
 namespace SIPView_PDF
 {
+
     public partial class Form1 : Form
     {
+        private ImGearDocument igDocument = null;
+        private int currentPageIndex = 0;
+
         public Form1()
         {
+            ImGearEvaluationManager.Initialize();
+            ImGearEvaluationManager.Mode = ImGearEvaluationMode.Watermark;
+
+            // Add support for PDF and PS files.
+            ImGearCommonFormats.Initialize();
+            ImGearFileFormats.Filters.Insert(0, ImGearPDF.CreatePDFFormat());
+            ImGearFileFormats.Filters.Insert(0, ImGearPDF.CreatePSFormat());
+            ImGearPDF.Initialize();
+
             InitializeComponent();
         }
+
+        private void imGearPageView1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialogLoad = new OpenFileDialog();
+            // Allow only PDF and PS files.
+            openFileDialogLoad.Filter =
+            ImGearFileFormats.GetSavingFilter(ImGearSavingFormats.PDF) + "|" +
+            ImGearFileFormats.GetSavingFilter(ImGearSavingFormats.PS);
+
+            if (DialogResult.OK == openFileDialogLoad.ShowDialog())
+            {
+                using (FileStream inputStream =
+                    new FileStream(openFileDialogLoad.FileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    try
+                    {
+                        // Load the entire the document.
+                        igDocument = ImGearFileFormats.LoadDocument(inputStream);
+                    }
+                    catch (ImGearException ex)
+                    {
+                        // Perform error handling.
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                // Render first page.
+                renderPage(0);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Check if document exists.
+            if (igDocument == null)
+                return;
+
+            string filename = "";
+            // Open File dialog. For this sample, just allow PDF or PS.
+            ImGearSavingFormats savingFormat = ImGearSavingFormats.PDF;
+
+            using (SaveFileDialog fileDialogSave = new SaveFileDialog())
+            {
+                fileDialogSave.Filter = ImGearFileFormats.GetSavingFilter(ImGearSavingFormats.PDF) + "|" +
+                ImGearFileFormats.GetSavingFilter(ImGearSavingFormats.PS);
+                fileDialogSave.OverwritePrompt = true;
+                if (fileDialogSave.ShowDialog(this) != DialogResult.OK)
+                {
+                    return;
+                }
+                filename = fileDialogSave.FileName;
+            }
+
+            // Save to output file.
+            using (FileStream outputStream = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite))
+            {
+                try
+                {
+                    // Save the page in the requested format.
+                    ImGearFileFormats.SaveDocument(
+                        igDocument,
+                        outputStream,
+                        0,
+                        ImGearSavingModes.OVERWRITE,
+                        savingFormat,
+                        null);
+                    MessageBox.Show("Saved {0}", filename);
+                }
+                // Perform error handling.
+                catch (Exception ex)
+                {
+                    MessageBox.Show(string.Format("Could not save file {0}. {1}", filename, ex.Message));
+                    return;
+                }
+            }
+        }
+
+        private void renderPage(int pageNumber)
+        {
+            // Create page to hold the content.
+            ImGearPage igPage = null;
+            // Load a single page from the loaded document.
+            try
+            {
+                igPage = igDocument.Pages[pageNumber];
+                if (igPage != null)
+                {
+                    // Create a new page display to prepare the page for being displayed.
+                    ImGearPageDisplay igPageDisplay = new ImGearPageDisplay(igPage);
+                    // Associate the page display with the page view.
+                    imGearPageView1.Display = igPageDisplay;
+                    // Cause the page view to repaint.
+                    imGearPageView1.Invalidate();
+                    currentPageIndex = pageNumber;
+                    toolStripStatusLabel1.Text = string.Format("{0} of {1}", pageNumber + 1, igDocument.Pages.Count);
+
+                }
+            }
+            catch (ImGearException ex)
+            {
+                // Perform error handling.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            ImGearPDF.Terminate();
+            imGearPageView1.Display = null;
+        }
+
+        private void nextPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (igDocument != null)
+                if (currentPageIndex < igDocument.Pages.Count - 1)
+                    renderPage(currentPageIndex + 1);
+        }
+
+        private void previousPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (igDocument != null)
+                if (currentPageIndex > 0)
+                    renderPage(currentPageIndex - 1);
+        }
+
+
     }
 }
